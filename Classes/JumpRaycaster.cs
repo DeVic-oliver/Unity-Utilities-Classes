88c5ab995bec6c41fc1476a789bef21141b27307
Victor Matheus <victormatheus183@gmail.com>
2022-12-18 17:49:45 -0300

    utils  updated
    package updated;
    
    JumpRaycaster 3D checks for the Ground layermask

--- Classes/JumpRaycaster.cs
+++ Classes/JumpRaycaster.cs
@@ -1,41 +1,55 @@
 ﻿using UnityEngine;
 
-public static class JumpRaycaster
+namespace Scripts.Utils
 {
-    /// <summary>
-    /// Fires a ray from the middle of the gameobject that returns a bool value if collides with another collider;
-    /// </summary>
-    /// <param name="gameObjectCollider">The gameobject collider that will be a jump check</param>
-    /// <returns>True if ray hits another collider | False if not hits another collider</returns>
-    public static bool CheckIfIsGrounded(Collider gameObjectCollider)
-    {
-        float distoToGround = gameObjectCollider.bounds.extents.y;
-        bool isGrounded = Physics.Raycast(gameObjectCollider.transform.position, Vector3.down, distoToGround + 0.1f);
-
-        if (isGrounded)
-        {
-            return true;
-        }
-
-        return false;
-    }
 
     /// <summary>
-    /// Fires a ray from the middle of the gameobject that returns a bool value if collides with another collider
+    /// Parses if the game object is touching the ground. The Ground needs to be targeted with a 'Layer Mask' named 'Ground'
     /// </summary>
-    /// <param name="gameObjectCollider">The gameobject collider that will be a jump check</param>
-    /// <param name="spaceBetweenColliderAndGround">A customizable distance between gameobject collider and 'ground' collider. Default space 0.1f</param>
-    /// <returns>True if ray hits another collider | False if not hits another collider</returns>
-    public static bool CheckIfIsGrounded(Collider gameObjectCollider, float spaceBetweenColliderAndGround = 0.1f)
-    {
-        float distoToGround = gameObjectCollider.bounds.extents.y;
-        bool isGrounded = Physics.Raycast(gameObjectCollider.transform.position, Vector3.down, distoToGround + spaceBetweenColliderAndGround);
 
-        if (isGrounded)
+    public static class JumpRaycaster
+    {
+        private static string _mask = "Ground";
+
+        /// <summary>
+        /// Fires a ray from the middle of the gameobject that returns a bool value if collides with another collider from a
+        /// gameobject with layer mask 'Ground' attached;
+        /// </summary>
+        /// <param name="gameObjectCollider">The gameobject collider that will be a jump check</param>
+        /// <returns>True if ray hits another collider | False if not hits another collider</returns>
+        public static bool CheckIfIsGrounded(Collider gameObjectCollider)
         {
-            return true;
+            float distoToGround = gameObjectCollider.bounds.extents.y;
+            var groundLayerMaks = LayerMask.GetMask(_mask);
+            bool hitGround = Physics.Raycast(gameObjectCollider.transform.position, Vector3.down, distoToGround + 0.1f, groundLayerMaks);
+
+            if (hitGround)
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        return false;
+        /// <summary>
+        /// Fires a ray from the middle of the gameobject that returns a bool value if collides with another collider from a
+        /// gameobject with layer mask 'Ground' attached;
+        /// </summary>
+        /// <param name="gameObjectCollider">The gameobject collider that will be a jump check</param>
+        /// <param name="spaceBetweenColliderAndGround">A customizable distance between gameobject collider and 'ground' collider. Default space 0.1f</param>
+        /// <returns>True if ray hits another collider | False if not hits another collider</returns>
+        public static bool CheckIfIsGrounded(Collider gameObjectCollider, float spaceBetweenColliderAndGround = 0.1f)
+        {
+            float distoToGround = gameObjectCollider.bounds.extents.y;
+            var groundLayerMaks = LayerMask.GetMask(_mask);
+            bool hitGround = Physics.Raycast(gameObjectCollider.transform.position, Vector3.down, distoToGround + spaceBetweenColliderAndGround, groundLayerMaks);
+
+            if (hitGround)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
